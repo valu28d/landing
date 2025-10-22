d71247dcd387e1bae062ebac1a13aa4e40d64c77
Valentina Falconi <valentinafalconi28@gmail.com>
2025-10-22 20:00:01 +0000

    Cambios Guia08

---
+++ src/js/file01.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const showToast = () => {
+    const toast = document.getElementById("toast-interactive");
+    if (toast) {
+        toast.classList.add("md:block");
+    }
+};
+
+const showVideo = () => {
+    const demo = document.getElementById("demo");
+    if (demo) {
+        demo.addEventListener("click", () => {
+            window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+        });
+    }
+};
+
+(() => {
+    showToast();
+    showVideo();
+})();
